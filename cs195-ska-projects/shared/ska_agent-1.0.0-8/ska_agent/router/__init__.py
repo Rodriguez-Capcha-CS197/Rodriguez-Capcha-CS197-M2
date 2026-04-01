70b86ddc917cf3ff0c5c8738d7847190b51d77a7
anusridh97 <100602167+anusridh97@users.noreply.github.com>
2026-04-01 11:33:43 -0700

    Delete cs195-ska-projects/shared/ska_agent-1.0.0-8 directory

--- cs195-ska-projects/shared/ska_agent-1.0.0-8/ska_agent/router/__init__.py
+++
@@ -1,3 +0,0 @@
-# Router components - require sentence-transformers/torch
-# Import directly when needed:
-# from ska_agent.router.adaptive_router import AdaptiveRouter
--- cs195-ska-projects/shared/ska_agent-1.0.0-8/ska_agent/training/__init__.py
+++
@@ -1,3 +0,0 @@
-# Training - requires torch
-# Import directly when needed:
-# from ska_agent.training.trainers import SKATrainer
